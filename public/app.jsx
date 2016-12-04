8386706bb3ed3e6ffc29748ae0999a3bfffa7593
“DovileSand” <dovile.sand@protonmail.ch>
2016-12-04 11:48:23 +0000

    Adding HelloReact basic project wit introductory setup and example files
    This commit adds basic React.js project. It can be used as an example how to get
    started with a React and can be later evolved into a more complex project or
    used as a React refresher. It contains a basic node server, essential
    dependancies in the `package.json` file and additional dependancies, such as
    `babel` in the script tags.
    
    It introduces examples for such concepts as "events callbacks" in React,
    "component properties", "component state", "nested components" and "aside props
    state".
    
    This mini project allows to enter a name and/or a message in one or two of the
    input fields and upon the button click it updates the greeting message. It is
    achieved by rendering a simple "container component" in the browser, which
    maintains the state of its two children components. One of them responds to the
    user input while another, "presentational component", displays the message.
    
    After cloning this project please run `npm install` in your command line and to
    start a server run `node server.js`.

---
+++ public/app.jsx
@@ -0,0 +1,86 @@
+var GreeterMessage = React.createClass({
+  render: function() {
+
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return(
+      <div>
+        <h4> Hello {name} !</h4>
+        <p> {message} </p>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+
+    var updates = {};
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
+
+    if (name.length  > 0) {
+      this.refs.name.value = '';
+      updates.name = name;
+    }
+
+    if (message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+    this.props.onNewData(updates);
+  },
+  render: function() {
+    return(
+      <form onSubmit={this.onFormSubmit} >
+        <input type= "text" ref="name" placeholder="Enter your name" />
+        <br/>
+        <textarea ref="message" placeholder="Enter your message" />
+        <br/>
+        <button> Update  </button>
+      </form>
+    );
+  }
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'No message was provided...'
+    };
+  },
+  getInitialState: function() {
+    return {
+      name: this.props.name,
+      message: this.props.message
+    }
+  },
+  handleNewData: function(updates) {
+    this.setState(updates);
+  },
+  render: function() {
+
+    var name = this.state.name;
+    var message = this.state.message;
+
+    return(
+      <div>
+        <GreeterMessage name={name} message={message} />
+        <GreeterForm onNewData={this.handleNewData} />
+      </div>
+    );
+  }
+});
+
+var firstName = 'Dovile'
+var message = 'This is a message!';
+
+ReactDOM.render(
+
+  <Greeter name = {firstName} message = {message} />,
+  document.getElementById('app')
+
+);
